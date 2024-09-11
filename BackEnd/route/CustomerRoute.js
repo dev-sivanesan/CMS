d0d624341ef12e736f58e57fea721bed2c26a2fa
dev-sivanesan <sivanesan.dev@gmail.com>
2024-09-11 20:08:18 +0530

    updated Customer routes

--- BackEnd/route/CustomerRoute.js
+++ BackEnd/route/CustomerRoute.js
@@ -1,12 +1,13 @@
 import express from "express"
 import checkToken from "../middleware/JwtMiddleware.js";
 import { checkRole } from "../middleware/checkRole.js";
-import { createUserController,getallUserController,updateUserController,deleteUserController} from "../controller/CustomerController.js";
+import { createUserController,getUserController,getallUserController,updateUserController,deleteUserController} from "../controller/CustomerController.js";
 
 const CustomerRouter = express.Router();
 
 CustomerRouter.post("/createUser", checkToken,checkRole(['Manager']), createUserController);
 CustomerRouter.get("/getAllUsers", checkToken,checkRole(["Manager","Supervisor","Worker"]), getallUserController);
+CustomerRouter.get("/getUser",checkToken,checkRole(["Manager","Supervisor","Worker"]),getUserController)
 CustomerRouter.put("/updateUser", checkToken, checkRole(["Manager","Supervisor"]),updateUserController);
 CustomerRouter.delete("/deleteUser", checkToken,checkRole(["Manager"]), deleteUserController);
 
