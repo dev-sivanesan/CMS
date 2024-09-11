cd8ed729458e2bee00002009a5320db6b09220a6
dev-sivanesan <sivanesan.dev@gmail.com>
2024-09-11 11:04:30 +0530

    commited

--- BackEnd/controller/loginController.js
+++ BackEnd/controller/loginController.js
@@ -1,5 +1,6 @@
 import express from "express";
 import User from "../model/userModel.js";
+import Roles from "../model/rolesModel.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
@@ -8,35 +9,47 @@ dotenv.config();
 const loginController = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ where: { email } });
+
+    const user = await User.findOne({
+      where: { email },
+      include: [{ model: Roles, attributes: ["role_name"] }], 
+    });
+
     if (!user) {
-      console.log("user not found");
-      return res.json({ message: "User not found" });
+      console.log("User not found");
+      return res.json({ message: "User not found", status: false });
     }
+
+    // Check if the password is valid
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.json({ message: "Invalid password" });
+      return res.json({ message: "Invalid password", status: false });
     }
+
     const token = jwt.sign(
-      { userId: user.id, roleId: user.role_id },
+      { userId: user.id, role: user.Role.role_name },
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
+
     res.cookie("token", token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 1,
-      secure: true,
+      maxAge: 1000 * 60 * 60 * 1, 
+      secure: process.env.NODE_ENV === "production",
       path: "/",
     });
+
+  
     return res.json({
       message: "Login successful",
-      role: user.role,
+      role: user.Role.role_name, 
       token,
       status: true,
     });
   } catch (error) {
     console.error(error);
     return res.json({ message: "Internal server error", status: false });
-  }
+  } 
 };
+
 export default loginController;
